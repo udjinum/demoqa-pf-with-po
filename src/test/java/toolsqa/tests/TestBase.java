dfde7a1894f951e04804d646549174e766950747
Udjin <j-zen@yandex.ru>
2021-11-24 21:32:09 +0300

    added attach file

--- src/test/java/toolsqa/tests/TestBase.java
+++ src/test/java/toolsqa/tests/TestBase.java
@@ -1,7 +1,12 @@
 package toolsqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import toolsqa.pages.RegistrationPage;
 
 public class TestBase {
@@ -9,6 +14,22 @@ public class TestBase {
     @BeforeAll
     public static void setUpBeforeAll() {
         Configuration.startMaximized = true;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
     RegistrationPage registrationPage = new RegistrationPage();
