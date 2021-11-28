d4e2ff33c09c8b7589dde5f8dba486a7f6761f97
Udjin <j-zen@yandex.ru>
2021-11-28 13:46:05 +0300

    structure refactoring

--- src/test/java/toolsqa/helpers/Attach.java
+++ src/test/java/toolsqa/helpers/Attach.java
@@ -1,4 +1,4 @@
-package helpers;
+package toolsqa.helpers;
 
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Attachment;
--- src/test/java/toolsqa/tests/TestBase.java
+++ src/test/java/toolsqa/tests/TestBase.java
@@ -2,7 +2,7 @@ package toolsqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import helpers.Attach;
+import toolsqa.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
