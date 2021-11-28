e3e091699332f11b4ce52757fecd48a741b73686
Udjin <j-zen@yandex.ru>
2021-11-28 15:28:45 +0300

    adding credentials

---
+++ src/test/java/toolsqa/config/Credentials.java
@@ -0,0 +1,11 @@
+package toolsqa.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({"classpath:config/credentials.properties"})
+public interface Credentials extends Config {
+
+    String login();
+    String password();
+    String url();
+}
--- src/test/java/toolsqa/tests/TestBase.java
+++ src/test/java/toolsqa/tests/TestBase.java
@@ -1,14 +1,45 @@
 package toolsqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import toolsqa.config.Credentials;
+import toolsqa.helpers.Attach;
 import toolsqa.pages.RegistrationPage;
+
+import static java.lang.String.format;
 
 public class TestBase {
 
+    public static Credentials credentials = ConfigFactory.create(Credentials.class);
+
     @BeforeAll
     public static void setUpBeforeAll() {
+        String login = credentials.login();
+        String password = credentials.password();
+        String url = credentials.url();
+
         Configuration.startMaximized = true;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.remote = format("https://%s:%s@%s",login,password,url);
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
     RegistrationPage registrationPage = new RegistrationPage();
